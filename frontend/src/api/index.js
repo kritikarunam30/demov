ebc65891a2e5be1c68ff58bbed09e780facb3a24
Karunam Kriti <kritikarunam3@gmail.com>
2026-05-17 18:53:10 +0530

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://demov.onrender.com",
+  baseURL: "https:/demov.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
