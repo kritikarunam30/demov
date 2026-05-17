76f627e97103c13629efde015caac29c317977c2
Karunam Kriti <kritikarunam3@gmail.com>
2026-05-17 17:59:42 +0530

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -7,11 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
+    allow_origins=[https://demov.onrender.com/
     ],
     allow_credentials=True,
     allow_methods=["*"],
